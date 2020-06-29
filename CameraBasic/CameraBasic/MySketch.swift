2d5ccc6cabdd4d3551d812c6b1f8a58db7a05454
Juan Lee <yosohs@gmail.com>
2020-06-29 16:19:11 -0700

    Correct examples

--- CameraBasic/CameraBasic/MySketch.swift
+++ CameraBasic/CameraBasic/MySketch.swift
@@ -4,27 +4,13 @@ import UIKit
 
 class MySketch: Sketch,SketchDelegate {
     var camera: Camera!
-    var vectors: [Vector] = []
-    var photos: [Image] = []
-    
+
     func setup() {
         self.camera = createCamera(FRONT)
     }
-    
+
     func draw() {
-        if vectors.count < 70 {
-            vectors.append(createVector(random(0,self.width),0,random(4,15)))
-            photos.append(camera.get())
-        }
-        for index in 0...vectors.count-1 {
-            fill(251,248,241)
-            rect(vectors[index].x-2, vectors[index].y-2, 35, 42)
-            self.image(photos[index],vectors[index].x,vectors[index].y,31,31)
-            vectors[index].y = vectors[index].y + vectors[index].z!
-            if vectors[index].y > self.height {
-                vectors[index] = createVector(random(0,self.width),0,random(4,15))
-                photos[index] = camera.get()
-            }
-        }
+        self.camera.show()
+        self.camera.size(self.width,self.height)
     }
 }
--- RainingPolaroid/RainingPolaroid/MySketch.swift
+++ RainingPolaroid/RainingPolaroid/MySketch.swift
@@ -1,19 +1,29 @@
 import SwiftProcessing
 import UIKit
 
-class MySketch: Sketch, SketchDelegate{
-    
+class MySketch: Sketch,SketchDelegate {
+    var camera: Camera!
+    var vectors: [Vector] = []
+    var photos: [Image] = []
+
     func setup() {
-        //Launches the Sketch in app mode
-        appMode()
-        //Launches the Sketch in augmented reality face mode. Works on iPhone X, XR, 11, 11 Pro, and iPad Pro Remove // on the next line to enable.
-        //faceMode()
+        self.camera = createCamera(FRONT)
     }
-    
+
     func draw() {
-        background(255,255,255)
-        fill(255, 0, 0)
-        circle(125, 125, 50)
+        if vectors.count < 70 {
+            vectors.append(createVector(random(0,self.width),0,random(4,15)))
+            photos.append(camera.get())
+        }
+        for index in 0...vectors.count-1 {
+            fill(251,248,241)
+            rect(vectors[index].x-2, vectors[index].y-2, 35, 42)
+            self.image(photos[index],vectors[index].x,vectors[index].y,31,31)
+            vectors[index].y = vectors[index].y + vectors[index].z!
+            if vectors[index].y > self.height {
+                vectors[index] = createVector(random(0,self.width),0,random(4,15))
+                photos[index] = camera.get()
+            }
+        }
     }
-    
 }
